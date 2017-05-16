9f68fe0f3f8fa1064865fed5c7f4d1b6000f9ac6
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-05-16 00:58:34 -0400

    More security edits

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -2,8 +2,16 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
   $http.get('https://api.wunderground.com/api/7c8eaaf84b5e5dd0/conditions/q/IN/New_Castle.json')
   .then(function(res) {
-    console.log(res.data)
-    $scope.weather = res.data.current_observation;
+    // console.log(res.data)
+    // $scope.weather_icon = res.data.current_observation.weather_icon
+    var weather = res.data.current_observation;
+    var url = weather.icon_url.slice(4);
+
+    weather.icon_url = "https" + url;
+
+    $scope.weather = weather;
+
+    console.log($scope.weather);
   })
 
 }])
