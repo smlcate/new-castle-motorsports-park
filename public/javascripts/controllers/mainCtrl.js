5d82898cafefd0df23332adc5ee5c4c8d11bff12
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-05-16 00:33:45 -0400

    Edit weather api call to secure

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -1,6 +1,6 @@
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $http.get('http://api.wunderground.com/api/7c8eaaf84b5e5dd0/conditions/q/IN/New_Castle.json')
+  $http.get('https://api.wunderground.com/api/7c8eaaf84b5e5dd0/conditions/q/IN/New_Castle.json')
   .then(function(res) {
     console.log(res.data)
     $scope.weather = res.data.current_observation;
