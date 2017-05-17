b2a40aafc310963d2935394ce04dcb78ede89c0f
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-05-16 21:55:09 -0400

    Add pages and some styles

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -2,8 +2,7 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
   $http.get('https://api.wunderground.com/api/7c8eaaf84b5e5dd0/conditions/q/IN/New_Castle.json')
   .then(function(res) {
-    // console.log(res.data)
-    // $scope.weather_icon = res.data.current_observation.weather_icon
+
     var weather = res.data.current_observation;
     var url = weather.icon_url.slice(4);
 
@@ -11,7 +10,22 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.weather = weather;
 
-    // console.log($scope.weather);
   })
+
+  // $(window).scroll(function() {
+  //   var windowY = $(window).height();
+  //   var scrolledY = $(window).scrollTop();
+  //   // console.log(windowY);
+  //   // console.log(scrolledY);
+  //
+  //   var image_url = '../images/newCastleTurn1_001.jpg';
+  //
+  //   if (scrolledY > windowY) {
+  //     image_url = '../images/NEW-CASTLE-MOTORSPORTS-PARK.jpg';
+  //   }
+  //
+  //   $('.contentDiv').css('background', "image(" + image_url + ")");
+
+  // })
 
 }])
